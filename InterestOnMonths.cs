75aa8c2253b9c83cb685c55bc47ab4564e8ed63e
muratBee01 <muratalibek01@gmail.com>
2022-12-30 18:59:43 +0600

    Add project files.

---
+++ InterestOnMonths.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percent
+
+
+{
+    internal class InterestOnMonths
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(Calculate(userInput));
+
+            Console.ReadKey();
+
+        }
+        public static double Calculate(string userInput)
+        {
+            string[] numArray = userInput.Split(new char[] { ' ' });//Возвращает строковый массив, содержащий подстроки данного экземпляра, разделенные элементами заданной строки или массива знаков Юникода.
+
+            double sumOfmoney = Double.Parse(numArray[0], System.Globalization.CultureInfo.InvariantCulture);
+            double percent = Double.Parse(numArray[1], System.Globalization.CultureInfo.InvariantCulture);
+            double months = Double.Parse(numArray[2], System.Globalization.CultureInfo.InvariantCulture);
+
+            return sumOfmoney*Math.Pow((1+(percent/100)/12), months);
+        }
+    }
+}
